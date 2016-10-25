ccf38df6df104aa69eba96fd5bb7bab450c0482c
JMedon <jakubmedon@gmail.com>
2016-10-25 14:40:53 +0200

    Create import_briefing.py

---
+++ import_briefing.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+table = BeautifulSoup(open('C:/age0.html','r').read()).find('table')
+df = pd.read_html(table) #I think it accepts BeatifulSoup object
+                         #otherwise try str(table) as input
